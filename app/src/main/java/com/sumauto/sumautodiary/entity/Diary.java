db88f71f9802f336708d9b381e43a098d500865e
summerEnd <771509565@qq.com>
2015-11-23 10:05:37 +0800

    sd

--- app/src/main/java/com/sumauto/sumautodiary/entity/Diary.java
+++ app/src/main/java/com/sumauto/sumautodiary/entity/Diary.java
@@ -6,14 +6,40 @@ import com.sumauto.sumautodiary.db.DBColumn;
  * Created by Lincoln on 2015/11/20.
  * diary
  */
-public class Diary
-{
+public class Diary {
+    /**
+     * 日记标题
+     */
     @DBColumn(value = DBColumn.text)
     private String title;
 
+    /**
+     * 日记的内容
+     */
     @DBColumn(value = DBColumn.text)
     private String content;
 
+    /**
+     * 日记的日期
+     */
     @DBColumn(value = DBColumn.text)
     private String date;
+
+    /**
+     * 日记的图片
+     */
+    @DBColumn(value = DBColumn.text)
+    private String images;
+
+    /**
+     * 天气
+     */
+    @DBColumn(value = DBColumn.text)
+    private String weather;
+
+    /**
+     * 心情
+     */
+    @DBColumn(value = DBColumn.text)
+    private String mood;
 }
---
+++ app/src/main/java/com/sumauto/sumautodiary/utils/DateUtils.java
@@ -0,0 +1,10 @@
+package com.sumauto.sumautodiary.utils;
+
+/**
+ * Created by Lincoln on 2015/11/23.
+ * 统一的日期处理
+ */
+public class DateUtils
+{
+    public static final String FULL_DATE_FORMATTER = "yyyy-MM-dd hh:mm:ss";
+}
